ecf13dbdeed8af54c609a43a22069edc87d229ee
Catherine <sailorv@gmail.com>
2025-01-07 20:46:23 -0600

    Your Eyes Only
    Added goku_patrol.exe, tweaked positioning of different elements, updated back buttons on your_eyes_only

---
+++ your_eyes_only/js/goku.js
@@ -0,0 +1,28 @@
+/* Goku Patrol */
+function setTime() {
+    // set the current hour
+    const now = new Date();
+    const hour = now.getHours();
+
+    // get the current time as a string
+    const currentTime = hour.toLocaleString();
+
+    if (document.getElementById('goku')) {
+        // make goku show if it's past 7
+        if (currentTime >= 19) {
+            document.getElementById('goku').style.display = 'block';
+        } else {
+            document.getElementById('goku').style.display = 'none';
+        }
+    }
+}
+
+/* Feed Goku */
+function feedGoku() {
+    document.getElementById('goku').remove();
+    document.getElementById('goku_actions').innerHTML = "Thank you for giving Goku a snack. Your gift of calories will help him protect this website."
+}
+
+
+// check regularly
+setInterval(setTime, 1000);
